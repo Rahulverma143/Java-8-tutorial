5f65ff26c6782cc600a4862f1ea47277aa216204
Rahulverma143 <rahulvearma17@gmail.com>
2026-01-30 01:04:06 +0530

    collect tp list method

--- src/stream/CollectToList.java
+++ src/stream/CollectToList.java
@@ -6,13 +6,18 @@ import java.util.stream.Collectors;
 
 public class CollectToList {
 	public static void main(String[] args) {
-		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+		List<Integer> numbers = Arrays.asList(1, 2,2, 3, 4, 5, 6);
+		System.out.println("even number print");
 		List<Integer> evenList = numbers
 				.stream()
 				.filter(n -> n % 2 == 0)
 				.collect(Collectors.toList());
 		
 		System.out.println(evenList);
+		
+	    System.out.println("full list print");
+		List<Integer> evenList1 = numbers.stream().collect(Collectors.toList());
+		System.out.println(evenList1);
 
 	}
 }
